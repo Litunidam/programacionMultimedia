66d9837653a76a577d2fd2514acdfa497e6db637
Manuel Martín Benito <manuelmarb@gmail.com>
2020-10-28 16:49:34 +0100

    example1

---
+++ Ejemplo1/app/src/main/java/com/example/ejemplo1/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.ejemplo1;
+
+
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button1;
+    private TextView tv;
+    private ConstraintLayout background;
+    private Button button2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button1 = findViewById(R.id.btn_hora);
+        tv = findViewById(R.id.tv_hora);
+        background = findViewById(R.id.cl_fondo);
+        button2 = findViewById(R.id.btn_back);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("Mira ste Jayce");
+                background.setBackgroundResource(R.drawable.jayce);
+
+
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                background.setBackgroundColor(0x0000FF00);
+                tv.setText("Holis");
+            }
+        });
+
+    }
+}
